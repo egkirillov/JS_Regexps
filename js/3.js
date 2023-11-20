0dbe0be5580785e1ac63896d6bbfb8b9bf67d383
egkirillov <>
2023-11-20 18:00:37 +0300

    task_3 fix2

--- js/3.js
+++ js/3.js
@@ -2,7 +2,7 @@ function task_3()
 {    
     function isValidstr(str) {
         var objRE =/^[0-9A-ZА-ЯЁ]+$/i;
-        return !objRE.test(str);
+        return objRE.test(str);
         }
 
     let str=prompt("введите строку");
